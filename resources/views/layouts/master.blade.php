d8cabe10462413aec6ce352cf3a464ba26cd90c4
spyder21er <spyder21er@gmail.com>
2019-08-29 22:54:36 +0800

    Added login page.

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html lang="en">
     <head>
-        <title>AGrS - @yield('title')</title>
+        <title>{{ config('app.name', 'Laravel') }} - @yield('title')</title>
 
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,12 @@
 |
 */
 
+Route::middleware('auth')->group(function () {
 Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
-
 Route::get('/home', 'HomeController@index')->name('home');
+});
+
+Auth::routes();
