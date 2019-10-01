33cedc5b21a0ab7b298183f2e5d95b88ea93fb9f
spyder21er <spyder21er@gmail.com>
2019-10-01 23:16:17 +0800

    Driver route security.

--- app/Http/Controllers/DriverController.php
+++ app/Http/Controllers/DriverController.php
@@ -6,7 +6,11 @@ use Illuminate\Http\Request;
 
 class DriverController extends Controller
 {
-    //
+    public function __construct()
+    {
+        $this->middleware('driver');
+    }
+
     public function index()
     {
         return view('driver.index');
