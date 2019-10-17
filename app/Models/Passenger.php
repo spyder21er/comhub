f5a7ffb1c4cebd74a736847e5d10356f69d5e641
Allen Mabana <spyder21er@gmail.com>
2019-10-17 15:29:22 +0800

    Update Passenger.php

--- app/Models/Passenger.php
+++ app/Models/Passenger.php
@@ -6,8 +6,14 @@ use App\Scopes\PassengerScope;
 
 class Passenger extends User
 {
+    /**
+     * The table associated with this model.
+     */
     protected $table = 'users';
 
+    /**
+     * Booting method and add global scope for this model.
+     */
     protected static function boot()
     {
         parent::boot();
