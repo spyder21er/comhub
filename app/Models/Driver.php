15cb2012ee69d64d1ebd8dc36549979b0f5a97e2
Allen Mabana <spyder21er@gmail.com>
2019-10-17 15:27:51 +0800

    DocBlock in Driver.php

--- app/Models/Driver.php
+++ app/Models/Driver.php
@@ -6,8 +6,16 @@ use App\Scopes\DriverScope;
 
 class Driver extends User
 {
+    /**
+     * Table associated with this model.
+     *
+     * @var string
+     */
     protected $table = 'users';
 
+    /**
+     * Booting method and add global scope for this model.
+     */
     protected static function boot()
     {
         parent::boot();
