f696aed6fc86d1c76888065d20e1148beb33685d
spyder21er <spyder21er@gmail.com>
2019-11-20 12:35:58 +0800

    Format.

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 build:
-	cp .env.sqlite .env
-	composer install
-	php artisan key:generate
-	touch database/database.sqlite
-	php artisan migrate:fresh --seed
+    cp .env.sqlite .env
+    composer install
+    php artisan key:generate
+    touch database/database.sqlite
+    php artisan migrate:fresh --seed
--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -72,7 +72,7 @@ class RegisterController extends Controller
             'last_name'     => $data['last_name'],
             'phone'         => $data['phone'],
             'email'         => $data['email'],
-            'password' => Hash::make($data['password']),
+            'password'      => Hash::make($data['password']),
             // only passengers can create account
             'role_id' => 4,
         ]);
